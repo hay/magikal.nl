b226519326eb3569e654731a50df0d54f554c513
Hay Kranen <hay@haykranen.nl>
2021-04-21 12:38:44 +0200

    Adding soial tags, improved the counter

--- app/js/index.js
+++ app/js/index.js
@@ -23,26 +23,34 @@
         const START_DATE = dayjs('2021-04-19');
         const START_COUNT = 4688307;
         const PER_DAY = 112549;
-        const SEC_PER_DAY = 24 * 60 * 60;
-        const PER_SECOND = PER_DAY / SEC_PER_DAY;
+        const SEC_PER_DAY = 24 * 60 * 60 * 2; // Only shots between 8am - 8pm, so double the amount
+        const PER_SECOND = (PER_DAY / SEC_PER_DAY);
 
         function getCount() {
             // Get difference in seconds, multiply by shots per second
             // and add to total
             const now = dayjs();
             const diff = now.diff(START_DATE, 'seconds');
-            const count = Math.round(START_COUNT + (diff * PER_SECOND));
-            return numberWithCommas(count);
+            return Math.round(START_COUNT + (diff * PER_SECOND));
         }
 
         function show() {
-            $("#counter-value").innerHTML = getCount();
+            function setCounter(val) {
+                $("#counter-value").innerHTML = numberWithCommas(val);
+            }
+
+            let count = getCount();
+            setCounter(count);
 
             // Vacinations only happen between 8am - 8pm...i think
             const hour = dayjs().hour();
 
             if (hour >= 8 && hour <= 22) {
-                setTimeout(show, (PER_SECOND / 2));
+                (function anim() {
+                    count += 1;
+                    setCounter(count);
+                    setTimeout(anim, PER_SECOND * 1000);
+                })();
             }
         }
 
