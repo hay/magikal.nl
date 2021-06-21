4a89818c711ecbb9dc4eee55dd700c5b478ca3e7
Hay Kranen <hay@haykranen.nl>
2021-06-21 11:19:42 +0200

    Everybody from 2003 or earlier can be vaccinated, so let's simplify this site

--- app/js/index.js
+++ app/js/index.js
@@ -1,107 +1,26 @@
 (function() {
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    }
-
     function track(cat, action) {
         if ('_paq' in window) {
             window._paq.push(['trackEvent', cat, action]);
         }
     }
 
-    class Counter {
-        constructor(opts) {
-            this.startDate = dayjs(opts.startDate);
-            this.startCount = opts.startCount;
-            this.perDay = opts.perDay;
-            this.secPerDay = 24 * 60 * 60 * 2; // Only shots between 8am - 8pm, so double the amount
-            this.perSecond = this.perDay / this.secPerDay;
-
-        }
-
-        getCount() {
-            // Get difference in seconds, multiply by shots per second
-            // and add to total
-            const now = dayjs();
-            const diff = now.diff(this.startDate, 'seconds');
-            return Math.round(this.startCount + (diff * this.perSecond));
-        }
-
-        isCounting() {
-            // Vacinations only happen between 8am - 8pm...i think
-            const hour = dayjs().hour();
-            return hour >= 8 && hour < 20;
-        }
-
-        nextShot(cb) {
-            setTimeout(cb, this.perSecond * 1000);
-        }
-    }
-
     new Vue({
         el : "#main",
-
-        computed : {
-            shotCountFormatted() {
-                return numberWithCommas(this.shotCount);
-            }
-        },
 
         data() {
             return {
                 aboutVisible : false,
-                birthyearValue : null,
-                birthyear : null,
-                counter : null,
-                currentMaxYear : 2002,
-                lastModified : null,
-                shotCount : null
+                lastModified : null
             };
         },
 
         methods : {
-            between(val, min, max) {
-                return val >= min && val <= max;
-            },
-
-            estimate() {
-                const year = Number(this.birthyearValue.trim());
-
-                if (isNaN(year)) {
-                    alert("Dat is geen geldig geboortejaar.");
-                    return;
-                }
-
-                if (year < 1900) {
-                    alert("Zo oud bent u echt niet.");
-                    return;
-                }
-
-                if (year > 2021) {
-                    alert("U bent nog niet geboren, dat lijkt me sterk.");
-                    return;
-                }
-
-                track('estimate-year', year);
-
-                this.birthyear = year;
-            },
-
             async getLastModified() {
                 const bust = String(Math.random()).slice(2);
                 const req = await window.fetch(`last-modified.txt?v=${bust}`);
                 const data = await req.text();
                 this.lastModified = data;
-            },
-
-            resetBirthyear() {
-                this.birthyearValue = null;
-                this.birthyear = null;
-            },
-
-            setShotCount() {
-                this.shotCount += 1;
-                this.counter.nextShot(() => this.setShotCount());
             },
 
             showAbout() {
@@ -111,18 +30,6 @@
         },
 
         mounted() {
-            this.counter = new Counter({
-                startCount : 13067373,
-                startDate : '2021-06-16',
-                perDay : 238845
-            });
-
-            this.shotCount = this.counter.getCount();
-
-            if (this.counter.isCounting()) {
-                this.setShotCount();
-            }
-
             this.getLastModified();
         }
     });
